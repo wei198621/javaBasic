9bf061db8781417b0617c93f725f37be87f598c5
wei198621 <wei198621@126.com>
2020-12-26 09:08:53 +0800

    sss

---
+++ baizhi_hu/test01output.java
@@ -0,0 +1,13 @@
+package day2;
+public class test01output{
+    public static void main(String[] args){
+	    int a=10;
+		System.out.println(a);
+		a=20;
+		System.out.println(a);
+		byte b =50;
+		long l=222222888888888888888888888888888888888888888888888;
+	}
+
+
+}
